0e445e91e6025ef995511fcf5fa2f4a942fe39df
joppy.nopp <jopper50698@gmail.com>
2024-10-14 15:57:46 +0700

    ver1

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';  
 // import GamePage from './GamePage';  // Correct import for the game page
-// import HowToPlayPage from './HowToPlayPage';  // Correct import for the how-to-play page
+import HowToPlayPage from './HowToPlayPage';  // Correct import for the how-to-play page
 import './App.css'; 
+import Select from './Select';
 
 function App() {
   
@@ -14,8 +15,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/game" element={<GamePage />} />
-        <Route path="/how-to-play" element={<HowToPlayPage />} /> */}
+        <Route path="/how-to-play" element={<HowToPlayPage />} />
+        <Route path="/select" element={<Select />} />
       </Routes>
     </Router>
   )
